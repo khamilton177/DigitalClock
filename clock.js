7bbd15a43c04491ad96bfd02449c42aa2901399e
khamilton177 <kerryjae@gmail.com>
2017-03-20 16:13:09 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -13,27 +13,32 @@ setInterval(function(){
   //  If hours are over 11 then it's PM
     if (testHrs>=12){
       amPM.innerHTML="PM";
-      bod.style.backgroundImage="url('./images/night.jpg')";
-      msg.innerHTML="Hope you're enjoying your evening";
+      if (testHrs>17){
+        bod.style.backgroundImage="url('./images/night.jpg')";
+        msg.innerHTML="Hope you're enjoying your evening";
+      }else{
+        bod.style.backgroundImage="url('./images/noon.jpg')";
+        msg.innerHTML="Good Afternoon!";
+      }
     }else {
       amPM.innerHTML="AM";
       bod.style.backgroundImage="url('./images/morning.jpg')";
-      msg.innerHTML="Good Morning Beautiful!";
+      msg.innerHTML="Good Morning Sunshine!";
     }
   var testMins= clock.getMinutes();
   //  If minutes are single digit add preceding '0'
-  if(testMins<10){
-    mins.innerHTML="0"+testMins;
-  }
-  else{
-    mins.innerHTML=testMins;
-  }
+    if(testMins<10){
+      mins.innerHTML="0"+testMins;
+    }
+    else{
+      mins.innerHTML=testMins;
+    }
   var testSecs= clock.getSeconds();
   //  If seconds are single digit add preceding '0'
-  if(testSecs<10){
-    secs.innerHTML="0"+testSecs;
-  }
-  else{
-    secs.innerHTML=testSecs;
-  }
+    if(testSecs<10){
+      secs.innerHTML="0"+testSecs;
+    }
+    else{
+      secs.innerHTML=testSecs;
+    }
 }, 1000)
